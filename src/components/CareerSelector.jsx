016251f5c8a291e503b58b326100830078d9f487
Paulo Araya <pauloaraya42@gmail.com>
2026-07-02 21:50:13 -0400

    feat: added iti jiji

--- src/components/CareerSelector.jsx
+++ src/components/CareerSelector.jsx
@@ -31,24 +31,31 @@ export default function CareerSelector({
 	return (
 		<div className="flex items-center justify-center w-full h-full">
 			<div
-				className="flex flex-col justify-evenly items-center rounded-2xl shadow-2xl w-100 h-80 bg-white"
+				className="flex flex-col justify-evenly items-center rounded-2xl shadow-2xl w-112 h-80 bg-white"
 				style={{ borderColor: "var(--color-border)", borderWidth: "4px" }}
 			>
 				<h1 className="text-4xl font-bold p-5">Elije tu carrera</h1>
-				<div className="flex flex-row w-full justify-around">
+				<div className="flex flex-row w-full justify-around px-2">
 					<button
-						className="cursor-pointer w-[40%] p-4 text-2xl rounded-2xl bg-blue-100"
+						className="flex justify-center items-center cursor-pointer w-[30%] p-4 text-2xl rounded-2xl bg-blue-100 "
 						onClick={() => sendCourseData("ICCI")}
 						disabled={loading !== null}
 					>
 						{loading === "ICCI" ? "Cargando..." : "ICCI"}
 					</button>
 					<button
-						className="cursor-pointer w-[40%] p-4 text-2xl rounded-2xl bg-orange-200 "
+						className="flex justify-center items-center cursor-pointer w-[30%] p-4 text-2xl rounded-2xl bg-orange-200 text-center"
 						onClick={() => sendCourseData("ICI")}
 						disabled={loading !== null}
 					>
 						{loading === "ICI" ? "Cargando..." : "ICI"}
+					</button>
+					<button
+						className="flex justify-center items-center cursor-pointer w-[30%] p-4 text-2xl rounded-2xl bg-green-100 text-center"
+						onClick={() => sendCourseData("ITI")}
+						disabled={loading !== null}
+					>
+						{loading === "ITI" ? "Cargando..." : "ITI"}
 					</button>
 				</div>
 				{error && <ErrorMessage error={error} />}
